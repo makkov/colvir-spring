536bbc4cac85f4cbc2a986e27875d72b08c516cf
makkov <maxim_86nva@mail.ru>
2024-05-20 20:21:47 +0300

    варианты настройки Pointcut'ов

--- src/main/java/com/colvir/spring/aop/aspects/LoggingAspect.java
+++ src/main/java/com/colvir/spring/aop/aspects/LoggingAspect.java
@@ -2,19 +2,40 @@ package com.colvir.spring.aop.aspects;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LoggingAspect {
 
-    @Before("execution(public void printAll())")
-    public void printAllLogging() {
-        System.out.println("Вызван метод printAll");
+    @Pointcut("execution(public void print*())")
+    public void printPatternMethod() {
+
     }
 
-    @Before("execution(public void print*())")
+    @Before("printPatternMethod()")
     public void printPatternLogging() {
         System.out.println("Вызван метод print*");
+    }
+
+    @Before("printPatternMethod()")
+    public void printPatternLogging2() {
+        System.out.println("Повторное напоминание: вызван метод print*");
+    }
+
+    @Before("execution(* *())")
+    public void printAllEmptyMethodsCall() {
+        System.out.println("Вызван метод без аргументов");
+    }
+
+    @Before("execution(* *(String, String))")
+    public void printTwoInputStringsCall() {
+        System.out.println("Вызван метод, который на входе принимает две строки");
+    }
+
+    @Before("execution(* *(..))")
+    public void printAllMethodsCall() {
+        System.out.println("Вызывается любой метод");
     }
 }
